06b5da7241cfaca1420f5747490af403bc248482
hayam999 <110453871+Hayam999@users.noreply.github.com>
2026-09-02 12:43:28 +0300

    feat: Solve variant C of problem 2 in lecture 8

--- lecture8/problem2.java
+++ lecture8/problem2.java
@@ -2,9 +2,90 @@ import tester.*;
 
 interface ILoNumber {
     boolean satisfying();
-    boolean satHelper(Conditions conds);
+    boolean satHelper(IConditions conds);
     boolean strictSatisfy();
-    boolean strictSatHelper(Conditions conds);
+    boolean strictSatHelper(IConditions conds);
+    boolean satStrictNoEx();
+    boolean satStrictNoExHelper(AlarmConditions conds);
+}
+interface IConditions{
+    boolean allGood();
+    IConditions updateConds(int num);
+    IConditions updateStrictConds(int num); 
+}
+
+abstract class AConditions implements IConditions{
+    boolean isEven;
+    boolean isPositiveAndOdd;
+    boolean from5To10;
+    boolean evenUpdated = false;
+    boolean PositiveUpdated = false;
+    boolean from5To10Updated = false;
+
+    AConditions(boolean isEven, boolean isPositiveAndOdd, boolean from5To10) {
+        this.isEven = isEven;
+        this.isPositiveAndOdd = isPositiveAndOdd;
+        this.from5To10 = from5To10;
+    }
+    public boolean allGood() {
+        return this.isEven && this.isPositiveAndOdd && this.from5To10;
+    }
+    public IConditions updateConds(int num) {
+        if (!evenUpdated) {
+            this.isEven = checkEven(num);
+            evenUpdated = isEven;
+        }
+        if (!PositiveUpdated) {
+            this.isPositiveAndOdd = checkPosAndOdd(num);
+            PositiveUpdated = isPositiveAndOdd;
+        }
+        if (!from5To10Updated) {
+            this.from5To10 = checkRange(num);
+            from5To10Updated = from5To10;
+        }
+        return this;
+    }
+
+    public IConditions updateStrictConds(int num) {
+        updateStrictCondsHelper(num);
+            return this;
+    }
+
+    public boolean updateStrictCondsHelper(int num) {
+        
+        if (!evenUpdated) {
+            this.isEven = checkEven(num);
+            if (isEven) {
+                evenUpdated = isEven;
+                return false;
+            }
+        }
+        if (!PositiveUpdated) {
+            this.isPositiveAndOdd = checkPosAndOdd(num);
+            if (isPositiveAndOdd) {
+                PositiveUpdated = isPositiveAndOdd;
+                return false;
+            }
+        }
+        if (!from5To10Updated) {
+            this.from5To10 = checkRange(num);
+            if (from5To10) {
+                from5To10Updated = from5To10;
+                return false;
+            }
+        } 
+        return true;
+    }
+
+    private boolean checkEven(int num) {
+        return num % 2 == 0;
+    }
+    private boolean checkPosAndOdd(int num) {
+        return (num > 0 && num % 2 != 0);
+    }
+    private boolean checkRange(int num) {
+        return (num >= 5 && num <= 10);
+    }
 }
 
 class MtLoNumber implements ILoNumber {
@@ -13,14 +94,20 @@ class MtLoNumber implements ILoNumber {
     public boolean satisfying() {
         return false;
     }
-    public boolean satHelper(Conditions conds) {
+    public boolean satHelper(IConditions conds) {
         return conds.allGood();
     }
-    public boolean strictSatHelper(Conditions conds) {
+    public boolean strictSatHelper(IConditions conds) {
         return conds.allGood();
     }
     public boolean strictSatisfy() {
         return false;
+    }
+    public boolean satStrictNoEx() {
+        return false;
+    }
+    public boolean satStrictNoExHelper(AlarmConditions conds) {
+        return conds.allGood();
     }
 }
 
@@ -38,7 +125,7 @@ class ConsLoNumber implements ILoNumber{
         return this.satHelper(conds);
     }
 
-    public boolean satHelper(Conditions conds) {
+    public boolean satHelper(IConditions conds) {
         if (conds.allGood()) {
             return true;
         }
@@ -51,84 +138,48 @@ class ConsLoNumber implements ILoNumber{
         return strictSatHelper(new Conditions(false, false, false));
     }
 
-    public boolean strictSatHelper(Conditions conds) {
+    public boolean strictSatHelper(IConditions conds) {
        if (conds.allGood()) {
         return true;
        } 
        else return this.rest.strictSatHelper(conds.updateStrictConds(this.first));
     }
+    
+    public boolean satStrictNoEx() {
+        return this.satStrictNoExHelper(new AlarmConditions(false, false, false, false));
+    }
+    public boolean satStrictNoExHelper(AlarmConditions conds) {
+        if (conds.alarmOn) {
+            return false;
+        }
+        else if (conds.allGood()) {
+            return true;
+        }
+        else {
+            return this.rest.satStrictNoExHelper(conds.updateCondsWithAlarm(this.first));
+        }
+    }
 
 }
 
-class Conditions {
-    boolean isEven;
-    boolean isPositiveAndOdd;
-    boolean from5To10;
-    boolean evenUpdated = false;
-    boolean PositiveUpdated = false;
-    boolean from5To10Updated = false;
-
+class Conditions extends AConditions {
     Conditions(boolean isEven, boolean isPositiveAndOdd, boolean from5To10) {
-        this.isEven = isEven;
-        this.isPositiveAndOdd = isPositiveAndOdd;
-        this.from5To10 = from5To10;
+        super(isEven, isPositiveAndOdd, from5To10);
     }
-    public boolean allGood() {
-        return this.isEven && this.isPositiveAndOdd && this.from5To10;
+}
+
+class AlarmConditions extends AConditions {
+    boolean alarmOn;
+    AlarmConditions(boolean isEven, boolean isPositiveAndOdd, boolean from5To10, boolean alarmOn) {
+        super(isEven, isPositiveAndOdd, from5To10);
+        this.alarmOn = alarmOn;
     }
-    public Conditions updateConds(int num) {
-        if (!evenUpdated) {
-            this.isEven = checkEven(num);
-            evenUpdated = isEven;
-        }
-        if (!PositiveUpdated) {
-            this.isPositiveAndOdd = checkPosAndOdd(num);
-            PositiveUpdated = isPositiveAndOdd;
-        }
-        if (!from5To10Updated) {
-            this.from5To10 = checkRange(num);
-            from5To10Updated = from5To10;
-        }
+
+    AlarmConditions updateCondsWithAlarm(int num) {
+        this.alarmOn = this.updateStrictCondsHelper(num);
         return this;
     }
 
-    public Conditions updateStrictConds(int num) {
-
-        if (!evenUpdated) {
-            this.isEven = checkEven(num);
-            if (isEven) {
-                evenUpdated = isEven;
-                return this;
-            }
-        }
-        if (!PositiveUpdated) {
-            this.isPositiveAndOdd = checkPosAndOdd(num);
-            if (isPositiveAndOdd) {
-                PositiveUpdated = isPositiveAndOdd;
-                return this;
-            }
-        }
-        if (!from5To10Updated) {
-            this.from5To10 = checkRange(num);
-            if (from5To10) {
-                from5To10Updated = from5To10;
-                return this;
-            }
-        } 
-        
-            return this;
-
-    }
-
-    private boolean checkEven(int num) {
-        return num % 2 == 0;
-    }
-    private boolean checkPosAndOdd(int num) {
-        return (num > 0 && num % 2 != 0);
-    }
-    private boolean checkRange(int num) {
-        return (num >= 5 && num <= 10);
-    }
 }
 
 class ExamplesNumbers {
@@ -136,6 +187,7 @@ class ExamplesNumbers {
     ILoNumber satisfyingList = new ConsLoNumber(6, new ConsLoNumber(5, empty));
     ILoNumber unSutisfyingList = new ConsLoNumber(3, new ConsLoNumber(4, empty));
     ILoNumber satisfyingStrict = new ConsLoNumber(5, new ConsLoNumber(6, new ConsLoNumber(6, empty)));
+    ILoNumber ListWithEx = new ConsLoNumber(6, new ConsLoNumber(5, new ConsLoNumber(42, new ConsLoNumber(6, empty))));
 
 
     boolean testSatisfyingList(Tester t) {
@@ -150,7 +202,13 @@ class ExamplesNumbers {
     boolean testsutisfyingStrict(Tester t) {
         return t.checkExpect(satisfyingStrict.strictSatisfy(), true);
     }
+    boolean testsutisfyingStrictWithEx(Tester t) {
+        return t.checkExpect(satisfyingStrict.satStrictNoEx(), true);
+    }
 
+    boolean testunsutisfyingStrictWithEx(Tester t) {
+        return t.checkExpect(ListWithEx.satStrictNoEx(), false);
+    }
     public static void main(String[] args) {
         // ============================================================
         // DEBUG MODE: use this when you want to set a breakpoint and
