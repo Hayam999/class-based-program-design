429f100e1f40cd78e4a9e98668fa28c2eeba437d
hayam999 <110453871+Hayam999@users.noreply.github.com>
2026-09-23 01:56:52 +0300

    Solve Entertainment.java problem

--- assignment4/src/Entertainment.java
+++ assignment4/src/Entertainment.java
@@ -9,65 +9,75 @@ interface IEntertainment {
     String format();
     //is this IEntertainment the same as that one?
     boolean sameEntertainment(IEntertainment that);
+    boolean sameMagazine(Magazine other);
+    boolean samePodcast(Podcast other);
+    boolean sameTVSeries(TVSeries other);
 }
 
-class Magazine implements IEntertainment {
+abstract class AnEntertainment implements IEntertainment {
     String name;
-    double price; //represents price per issue
-    String genre;
-    int pages;
-    int installments; //number of issues per year
-    
-    Magazine(String name, double price, String genre, int pages, int installments) {
+    double price;
+    int installments;
+
+    AnEntertainment(String name, double price, int installments) {
         this.name = name;
         this.price = price;
-        this.genre = genre;
-        this.pages = pages;
         this.installments = installments;
     }
+
     
-    //computes the price of a yearly subscription to this Magazine
+    public String format() {
+        return this.name + " ," + this.price + ".";
+    }
     public double totalPrice() {
         return this.price * this.installments;
+    }
+    public int duration() {
+        return 50 * this.installments;
+    }
+    public boolean sameMagazine(Magazine other) {
+        return false;
+    }
+    public boolean samePodcast(Podcast other) {
+        return false;
+    }
+    public boolean sameTVSeries(TVSeries other) {
+        return false;
+    }
+}
+
+class Magazine extends AnEntertainment {
+    String genre;
+    int pages;
+    
+    Magazine(String name, double price, String genre, int pages, int installments) {
+        super(name, price, installments);
+        this.genre = genre;
+        this.pages = pages;
     }
     
     //computes the minutes of entertainment of this Magazine, (includes all installments)
     public int duration() {
-        return 0;
+        return 5 * this.pages;
     }
     
     //is this Magazine the same as that IEntertainment?
     public boolean sameEntertainment(IEntertainment that) {
-        return true;
+        return that.sameMagazine(this);
     }
     
-    //produce a String that shows the name and price of this Magazine
-    public String format() {
-        return "";
+    public boolean sameMagazine(Magazine other) {
+        return this.name == other.name && this.pages == other.pages && this.price == other.price
+               && this.genre == other.genre && this.installments == other.installments;
     }
 }
 
-class TVSeries implements IEntertainment {
-    String name;
-    double price; //represents price per episode
-    int installments; //number of episodes of this series
+class TVSeries extends AnEntertainment{
     String corporation;
     
     TVSeries(String name, double price, int installments, String corporation) {
-        this.name = name;
-        this.price = price;
-        this.installments = installments;
+        super(name, price, installments);
         this.corporation = corporation;
-    }
-    
-    //computes the price of a yearly subscription to this TVSeries
-    public double totalPrice() {
-        return this.price * this.installments;
-    }
-    
-    //computes the minutes of entertainment of this TVSeries
-    public int duration() {
-        return 0;
     }
     
     //is this TVSeries the same as that IEntertainment?
@@ -75,41 +85,25 @@ class TVSeries implements IEntertainment {
         return true;
     }
     
-    //produce a String that shows the name and price of this TVSeries
-    public String format() {
-        return "";
+    public boolean sameTVSeries(TVSeries other) {
+        return this.name == other.name && this.installments == other.installments &&
+               this.corporation == other.corporation && this.price == other.price;
     }
 }
 
-class Podcast implements IEntertainment {
-    String name;
-    double price; //represents price per issue
-    int installments; //number of episodes in this Podcast
+class Podcast extends AnEntertainment {
     
     Podcast(String name, double price, int installments) {
-        this.name = name;
-        this.price = price;
-        this.installments = installments;
+        super(name, price, installments);
     }
-    
-    //computes the price of a yearly subscription to this Podcast
-    public double totalPrice() {
-        return this.price * this.installments;
-    }
-    
-    //computes the minutes of entertainment of this Podcast
-    public int duration() {
-        return 0;
-    }
-    
+     
     //is this Podcast the same as that IEntertainment?
     public boolean sameEntertainment(IEntertainment that) {
-        return true;
+        return that.samePodcast(this);
     }
-    
-    //produce a String that shows the name and price of this Podcast
-    public String format() {
-        return "";
+     
+    public boolean samePodcast(Podcast other) {
+        return this.name == other.name && this.price == this.price && this.installments == other.installments;
     }
 }
 
@@ -117,12 +111,42 @@ class ExamplesEntertainment {
     IEntertainment rollingStone = new Magazine("Rolling Stone", 2.55, "Music", 60, 12);
     IEntertainment houseOfCards = new TVSeries("House of Cards", 5.25, 13, "Netflix");
     IEntertainment serial = new Podcast("Serial", 0.0, 8);
-    
+    IEntertainment micky = new Magazine( "Micky mouse", 5.5, "Cartoon", 130, 8);
+    IEntertainment wednesday = new TVSeries("Wednesday", 7, 4, "Netflix");
+    IEntertainment roza = new Podcast("Rosa in the street", 70, 20);
+    IEntertainment otherSerial = new Podcast("Serial", 0.0, 8);
     //testing total price method
     boolean testTotalPrice(Tester t) {
         return t.checkInexact(this.rollingStone.totalPrice(), 2.55*12, .0001) 
         && t.checkInexact(this.houseOfCards.totalPrice(), 5.25*13, .0001)
         && t.checkInexact(this.serial.totalPrice(), 0.0, .0001);
     }
-    
+
+    boolean testSameEntertainment(Tester t) {
+        return t.checkExpect(roza.sameEntertainment(houseOfCards), false) &&
+               t.checkExpect(micky.sameEntertainment(micky), true) &&
+               t.checkExpect(serial.sameEntertainment(otherSerial), true);
+    }
+
+    public static void main(String[] args) {
+        // ============================================================
+        // DEBUG MODE: use this when you want to set a breakpoint and
+        // step through a specific method. Tester.runReport is NOT used
+        // here, so there's no 60ms watchdog timeout to fight with the
+        // debugger -- you can pause for as long as you want.
+        //
+        // To use it: comment out the "NORMAL MODE" line below, uncomment
+        // this block, edit the method call to whatever you're currently
+        // debugging, put your breakpoint inside that method, then hit
+        // Debug (F5) on this file/config.
+        // ------------------------------------------------------------
+         ExamplesBagel bagel = new ExamplesBagel();
+         boolean result = bagel.bagelInOunc1.sameRecipe(bagel.bagelInOunc1);
+         System.out.println(result);
+        // ============================================================
+
+        // NORMAL MODE: runs the full test suite (has the 60ms timeout,
+        // fine for a plain run, awkward if you're paused at a breakpoint)
+        Tester.runReport(new ExamplesBagel(), false, false);
+    }
 }
